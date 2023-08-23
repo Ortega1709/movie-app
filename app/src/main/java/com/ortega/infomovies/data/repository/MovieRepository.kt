86e68f2c0bc786845870f26c5652eb6212944334
ortega <kabweortega@gmail.com>
2023-08-23 17:09:42 +0200

    [Feat] Ajout MovieRepository
    MovieRepository contains all function for retrieve movies

--- app/src/main/java/com/ortega/infomovies/data/repository/MovieRepository.kt
+++ app/src/main/java/com/ortega/infomovies/data/repository/MovieRepository.kt
@@ -1,29 +1,68 @@
 package com.ortega.infomovies.data.repository
 
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.ortega.infomovies.data.datasource.local.MovieDatabase
+import com.ortega.infomovies.data.datasource.mediators.PopularMovieRemoteMediator
+import com.ortega.infomovies.data.datasource.mediators.RatedMovieRemoteMediator
+import com.ortega.infomovies.data.datasource.mediators.UpcomingMovieRemoteMediator
 import com.ortega.infomovies.data.datasource.remote.MovieApi
+import com.ortega.infomovies.domain.model.PopularMovie
+import com.ortega.infomovies.domain.model.RatedMovie
+import com.ortega.infomovies.domain.model.UpcomingMovie
+import com.ortega.infomovies.util.Constants.PER_PAGE
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class MovieRepository @Inject constructor(private val movieAPI: MovieApi) {
-
-    suspend fun getPopularMovies(page: Int, language: String) =
-        movieAPI.getPopularMovies(page = page, language = language)
-
-    suspend fun getRatedMovies(page: Int, language: String) =
-        movieAPI.getRatedMovies(page = page, language = language)
-
-    suspend fun getUpcomingMovies(page: Int, language: String) =
-        movieAPI.getUpcomingMovies(page = page, language = language)
-
-    suspend fun getRecommendedMovies(movieId: Int, page: Int, language: String) =
-        movieAPI.getRecommendedMovies(movieId = movieId, page = page, language = language)
+@OptIn(ExperimentalPagingApi::class)
+class MovieRepository @Inject constructor(
+    private val movieAPI: MovieApi,
+    private val movieDatabase: MovieDatabase
+) {
 
 
-    suspend fun getDetailMovies(movieId: Int, language: String) =
-        movieAPI.getDetailMovies(movieId = movieId, language = language)
+    fun getAllPopularMovies(): Flow<PagingData<PopularMovie>> {
 
+        val popularMovieSourceFactory = { movieDatabase.popularMovieDao().getAllMovies() }
+        return Pager(
+            config = PagingConfig(pageSize = PER_PAGE),
+            remoteMediator = PopularMovieRemoteMediator(
+                movieApi = movieAPI,
+                movieDatabase = movieDatabase
+            ),
+            pagingSourceFactory = popularMovieSourceFactory
+        ).flow
 
-    suspend fun searchMovies(query: String, page: Int, language: String) =
-        movieAPI.searchMovies(query = query, page = page, language = language)
+    }
 
+    fun getAllRatedMovies(): Flow<PagingData<RatedMovie>> {
+
+        val ratedMovieSourceFactory = { movieDatabase.ratedMovieDao().getAllMovies() }
+        return Pager(
+            config = PagingConfig(pageSize = PER_PAGE),
+            remoteMediator = RatedMovieRemoteMediator(
+                movieApi = movieAPI,
+                movieDatabase = movieDatabase
+            ),
+            pagingSourceFactory = ratedMovieSourceFactory
+        ).flow
+
+    }
+
+    fun getAllUpcomingMovies(): Flow<PagingData<UpcomingMovie>> {
+
+        val upcomingMovieSourceFactory = { movieDatabase.upcomingMovieDao().getAllMovies() }
+        return Pager(
+            config = PagingConfig(pageSize = PER_PAGE),
+            remoteMediator = UpcomingMovieRemoteMediator(
+                movieApi = movieAPI,
+                movieDatabase = movieDatabase
+            ),
+            pagingSourceFactory = upcomingMovieSourceFactory
+        ).flow
+
+    }
 
 }
